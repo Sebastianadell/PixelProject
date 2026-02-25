c681d2c814af4b3fa346baf89d04828b87353a23
AliS-05 <sefrali05@gmail.com>
2026-02-24 20:16:46 -0600

    added javafx color picker.

--- src/main/java/com/test/drawingcanvas/HelloController.java
+++ src/main/java/com/test/drawingcanvas/HelloController.java
@@ -3,18 +3,31 @@ package com.test.drawingcanvas;
 import javafx.fxml.FXML;
  import javafx.scene.canvas.GraphicsContext;
  import javafx.scene.canvas.Canvas;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
 
 
 public class HelloController {
-    public Mode curMode = Mode.Pencil; //Default to pencil mode
+    private Color curColor = Color.BLACK; // default to black pencil on white canvas
+    private Mode curMode = Mode.Pencil; //Default to pencil mode
+    @FXML
+    private ColorPicker colorPicker;
+
     @FXML
     private Canvas canvas;
     GraphicsContext gc;
+
     @FXML
     public void initialize() {
         gc = canvas.getGraphicsContext2D();
+        gc.setStroke(curColor);
+
+        colorPicker.setOnAction(e -> {
+            curColor = colorPicker.getValue();
+            gc.setStroke(curColor);
+            System.out.println("Current Color: " + curColor);
+        });
 
         canvas.setOnMousePressed(e -> {
             gc.beginPath();
@@ -33,10 +46,11 @@ public class HelloController {
                 case Fill -> {
                     // fill logic later
                 }
-
             }
         });
     }
+
+
 
     @FXML
     public void selectPencil(){
